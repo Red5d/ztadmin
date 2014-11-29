c777bfdb002186686f2b41020fe1b0d72ac65764
Red5d <daniel.colson99@gmail.com>
2014-11-29 17:44:02 -0500

    Initial commit. Added ztadmin.py and updated README.

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 ztadmin
 =======
 
-A scriptable interface to ZeroTier One's unofficial administration API.
+[ZeroTier One](https://www.zerotier.com/) is a Software Defined Networking service that allows for creating a
+scalable, secure P2P virtual network between all your computers no matter where they are.
+
+This tool manipulates the settings on the [admin panel](https://www.zerotier.com/admin.html) and can be scripted.
+
+
+##Requirements:
+* Python version 2.6+ (not 3.x)
+* Python [requests](https://pypi.python.org/pypi/requests/) module.
+* A ZeroTier One account.
+
+Run the script with the "help" option to show the usage information:
+Example: `python ztadmin.py help`
+If run with no parameters, the script will prompt you to log in, and will then
+display a listing of your networks and their members.
+
+##Current features:
+* Create a network.
+* Set attributes of a network or member of a network.
+* Authorize all pending join requests for a network (useful for mass-deployments).
+* Show network settings and information for a specified network ID.
+* Read login creds from a file named ".ztlogin" in the same directory (enables using the tool in automated scripts).
+
+The ".ztlogin" file should be in .ini format like this:
+```
+[ZeroTier]
+username=bob
+password=supersecret
+```
+
+##Future features:
+I'll be working on making the tool return more precise information when actions are applied (return network id 
+when a network is created, for example) that will be useful when using the tool in scripts.
+
+An "importable" python module to use the ZeroTier API is in the works as well.
+
+I haven't personally used ZeroTier One on a large scale or part of an infrastructure, but I know people do.
+If you think of a feature that would be useful in that environment, submit an issue or pull request and I'll see what I can do.
+I will probably do a little bit more polishing of the existing code though before accepting pull requests in case I change 
+some things.
---
+++ ztadmin.py
@@ -0,0 +1,182 @@
+#! /usr/bin/env python
+
+import sys, requests, json, getpass, pprint, ConfigParser
+
+# Create session for login.
+s = requests.Session()
+
+# Set ZeroTier API URL variables.
+ztAuth_URL = 'https://www.zerotier.com/api/auth/local'
+ztNetworkData_URL = 'https://www.zerotier.com/api/network'
+ztCreateNetwork_URL = 'https://www.zerotier.com/api/task/createNetwork'
+
+# Check for .ztlogin config file with login info (so user doesn't have to keep typing username/pw for repeated operations)
+config = ConfigParser.ConfigParser()
+c = config.read('.ztlogin')
+
+user = ""
+pw = ""
+
+if len(c) != 0:
+    user = config.get('ZeroTier', 'username')
+    pw = config.get('ZeroTier', 'password')
+
+# Login to ZeroTier.
+def login():
+    global user
+    global pw
+    if user == "":
+        user = raw_input("Username: ")
+        pw = getpass.getpass()
+    
+    info = {
+        'username': user,
+        'password': pw
+    }
+    
+    l = s.post(ztAuth_URL, data=info)
+    
+    if l.ok == True:
+        print "Login Successful."
+    else:
+        print "Login failed!"
+        login()
+    
+# Retrieve all network data.
+def getData():
+    r = s.get(ztNetworkData_URL)
+    data = json.loads(r.text)
+    return data
+
+# Retrieve data for specific network.
+def getNetwork(networkID):
+    r = s.get(ztNetworkData_URL+"/"+networkID)
+    network = json.loads(r.text)
+    return network
+
+# List network names and members.
+def list_networks():
+    data = getData()
+    
+    print "Networks:"
+    for network in data.keys():
+        print data[network]['desc']+" ("+network+"):"
+        for member in data[network]['/member'].keys():
+            print "\t"+data[network]['/member'][member]['id']+"\t"+data[network]['/member'][member]['ipAssignments']+"\t"+data[network]['/member'][member]['notes']
+        
+# Set/change an attribute on a network member.
+def setMemberData(networkID, memberID, attribute, value):
+    memberData = getData()[networkID]['/member'][memberID]
+    allowedAttributes = ["ipAssignments", "activeBridge", "authorized", "notes"]
+    
+    if attribute not in allowedAttributes:
+        print "Invalid attribute '"+attribute+"'."
+        print "Allowed attributes: "+str(allowedAttributes)
+        s.close()
+        sys.exit()
+    
+    memberData[attribute] = value
+
+    r = s.post(ztNetworkData_URL+"/"+networkID+'/member/'+memberID, data=memberData)
+    
+    if r.ok:
+        print "Set "+str(attribute)+" for "+str(memberID)+" to "+str(value)+"."
+    else:
+        print "Failed to set "+str(attribute)+" for "+str(memberID)+"."
+        
+# Create a new network.
+def createNetwork(name):
+    info = { "name": name }
+    
+    r = s.post(ztCreateNetwork_URL, data=info)
+    
+    if r.status_code == 409:
+        # Network name already exists.
+        print r.text
+    elif r.status_code != 200:
+        print "Failed to create network!"
+    else:
+        print "Network '"+name+"' created."
+
+# Set/change an attribute on a network.
+def setNetworkData(networkID, attribute, value):
+    # Retrieve network data and create array of writable attributes to check against.
+    networkData = getNetwork(networkID)
+    allowedAttributes = ["name", "desc", "enableBroadcast", "etherTypes", "infrastructure", "multicastLimit", "private", "v4AssignMode", "v4AssignPool", "v6AssignMode", "v6AssignPool"]
+    
+    # Check if the specified attribute is writable (included in allowedAttributes array).
+    if attribute not in allowedAttributes:
+        print "Invalid attribute '"+attribute+"'."
+        print "Allowed attributes: "+str(allowedAttributes)
+    else:
+        networkData[attribute] = value
+        r = s.post(ztNetworkData_URL+"/"+networkID, data=networkData)
+        
+        if r.ok:
+            print "Set "+str(attribute)+" for "+str(networkID)+" to "+str(value)+"."
+        else:
+            print "Failed to set "+str(attribute)+" for "+str(networkID)+"."
+            
+def authorizeAll(networkID):
+    # Authorize all computers asking to join a networkID (useful for mass-deployments)
+    data = getData()
+    for member in data[networkID]['/member'].keys():
+        setMemberData(networkID, member, "authorized", 1)
+            
+# Show info for specified network.
+def networkInfo(networkID):
+    network = getNetwork(networkID)
+    del network['/member']
+    pprint.pprint(network)
+
+
+# Begin main program.
+#
+# Log into ZeroTier and parse command options.
+
+if len(sys.argv) == 1:
+    login()
+    list_networks()
+    
+elif sys.argv[1] == "create":
+    login()
+    createNetwork(sys.argv[2])
+    
+elif sys.argv[1] == "setmember":
+    if len(sys.argv) == 6:
+        login()
+        setMemberData(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    else:
+        print "Usage: setmember <networkid> <memberid> <attribute> <value>"
+        
+elif sys.argv[1] == "setnetwork":
+    if len(sys.argv) == 5:
+        login()
+        setNetworkData(sys.argv[2], sys.argv[3], sys.argv[4])
+    else:
+        print "Usage: setnetwork <networkid> <attribute> <value>"
+        
+elif sys.argv[1] == "authall":
+    if len(sys.argv) == 3:
+        login()
+        authorizeAll(sys.argv[2])
+        
+elif sys.argv[1] == "networkinfo":
+    if len(sys.argv) == 3:
+        login()
+        networkInfo(sys.argv[2])
+    else:
+        print "Usage: networkinfo <networkid>"
+        
+else:
+    # If no valid command options found, print usage."
+    print "Usage: <command> <options>..."
+    print ""
+    print "create\t\t<name>\t\t\t\t\t\tCreate a new network."
+    print "setmember\t<networkid> <memberid> <attribute> <value>\tSet/change an attribute on a network member."
+    print "setnetwork\t<networkid> <attribute> <value>\t\t\tSet/change an attribute on a network."
+    print "authall\t\t<networkid>\t\t\t\t\tAuthorize all computers asking to join a network id."
+    print "networkinfo\t<networkid>\t\t\t\t\tShow info for a specified network."
+
+s.close()
+
